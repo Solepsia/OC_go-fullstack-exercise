23c70c6ab7b2b2088ad66eac5c02243e0bdbde30
Solepsia <solepsia@gmail.com>
2022-07-11 00:40:41 +0200

    exercice reussi ! :D

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const Product = require('./models/product');
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Solepsia:1abck2rz3452@crud-exercise.0y8ee.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true})
+    .then(() => console.log('Successfully connected to MongoDB.'))
+    .catch(() => console.log('Connection to MongoDB failed.'));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.get('/api/products', (req, res, next) => {
+    Product.find()
+        .then(products => res.status(200).json({ products: products }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({ _id: req.params.id })
+        .then(product => res.status(200).json({ product: product }))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.post('/api/products', (req, res, next) => {
+    const product = new Product({ ...req.body });
+    product.save()
+        .then(() => res.status(201).json({ product: product }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Modified!' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/products/:id', (req, res, next) => {
+    Product.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Deleted!' }))
+        .catch(rror => res.status(400).json({ error }));
+});
+
+module.exports = app;
